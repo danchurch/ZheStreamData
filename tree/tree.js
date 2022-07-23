335bc05c23b700b6c009d5faa80459095307b6a0
danchurch <danchurchthomas@gmail.com>
2022-07-23 23:09:46 +0200

    parse sample newick

--- tree/tree.js
+++ tree/tree.js
@@ -1,15 +1,20 @@
 window.addEventListener("load", main);
 
 function main(){
-d3.select('#plot').append('p').text('zoop')
+  d3.text("grahamAllSeqsTreeRelabeled.nwk").then(function(d){
 
-function parseNewick(a){
-  for(var e=[],r={},s=a.split(/\s*(;|\(|\)|,|:)\s*/),t=0;t<s.length;t++)
-    {var n=s[t];switch(n){case"(":var c={};
-    r.branchset=[c],e.push(r),r=c;break;case",":var c={};
-    e[e.length-1].branchset.push(c),r=c;break;case")":r=e.pop();
-    break;
-    case":":break;default:var h=s[t-1];")"==h||"("==h||","==h?r.name=n:":"==h&&(r.length=parseFloat(n))}}
-  return r}
-}
+    // borrowed from Jason Davies:
+    function parseNewick(a){
+      for(var e=[],r={},s=a.split(/\s*(;|\(|\)|,|:)\s*/),t=0;t<s.length;t++)
+        {var n=s[t];switch(n){case"(":var c={};
+        r.branchset=[c],e.push(r),r=c;break;case",":var c={};
+        e[e.length-1].branchset.push(c),r=c;break;case")":r=e.pop();
+        break;
+        case":":break;default:var h=s[t-1];")"==h||"("==h||","==h?r.name=n:":"==h&&(r.length=parseFloat(n))}}
+      return r}
+    
+    let zoop = parseNewick(d);
+    
+    console.log(zoop);
 
+})}
